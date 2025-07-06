65064d9042e6f0a37ee7f912ae60045e353fe593
LaoLujan <100359724+LaoLujan@users.noreply.github.com>
2025-07-06 16:12:45 +0100

    Gardener
    Class extended to abstract class and implemented with some behavior.
    I used the methods to perform actions.

--- The Kools/src/the/kools/people/Gardener.java
+++ The Kools/src/the/kools/people/Gardener.java
@@ -4,11 +4,22 @@
  * and open the template in the editor.
  */
 package the.kools.people;
+ 
+import the.kools.interfaces.Walking; // Walking interface imported, a Gardener could also Drive or use any behavior interface
 
 /**
  *
  * @author NHLUJ
  */
-public class Gardener {
+public class Gardener extends Person implements Walking { //Here I am specifying I will need the superClass "Person" which have information I need
+    public Gardener (String name, int age){
+    super (name, age);  //This super is the constructor door I created in Person.java to access to the data
+    }
     
+   public void workingOnMood(){  //this two publc voids methods means the actual performance of the action, like working, walking, driving
+       System.out.println(name + "is gardening rigth now");// print it for demostation but I think is not working anyway
+   }
+   public void walk(){
+       System.out.println(name + "Is walking around the garden");
+   }
 }
