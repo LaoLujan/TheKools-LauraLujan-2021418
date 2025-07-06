86761cfb7a7a3655f0ac0918b2b037613f845853
LaoLujan <100359724+LaoLujan@users.noreply.github.com>
2025-07-06 16:50:59 +0100

    First Item: Tool
    I defined a tool to work with

--- The Kools/src/the/kools/items/Tool.java
+++ The Kools/src/the/kools/items/Tool.java
@@ -9,6 +9,10 @@ package the.kools.items;
  *
  * @author NHLUJ
  */
-public class Tool {
+public class Tool extends Item {  //This Tool class is a subclass of Item, thats why we extend it to it
+    public Tool(){ 
+    super ("Tool"); // I specified in the Item class that item shloud have a name, but is not defined there, is defined here.
+                       //So I call the super class and I give it a name
+        }
     
 }
