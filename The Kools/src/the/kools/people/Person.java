5c7bbfa231e16810ac3694f4d1550cf6a9973c7d
LaoLujan <100359724+LaoLujan@users.noreply.github.com>
2025-07-25 13:19:22 +0100

    ADDING LINKS

--- The Kools/src/the/kools/people/Person.java
+++ The Kools/src/the/kools/people/Person.java
@@ -1,11 +1,12 @@
-/*/*FIND THE GITHUB REPO HERE: https://github.com/LaoLujan/TheKools-LauraLujan-2021418
+/*FIND THE GITHUB REPO HERE: https://github.com/LaoLujan/TheKools-LauraLujan-2021418
+/*VIDE LINK HERE: https://drive.google.com/file/d/1bw2iDyPN2pJSJ4KTXwRe8laZBUhq0hWv/view?usp=sharing
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package the.kools.people;
 
-/**
+/*
  *
  * @author NHLUJ
  */  
